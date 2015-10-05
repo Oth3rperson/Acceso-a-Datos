f30561e55ac6939c839f8ee668ec34b35ce9af70
Roberto <Roberto@Roberto-PC>
2015-10-05 18:37:45 +0200

    3 y 5

--- ficheros/src/ejercicio3/Ejercicio3.java
+++ ficheros/src/ejercicio3/Ejercicio3.java
@@ -8,45 +8,40 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 	public static void main(String[] args) {
-        
         try {
-        	
         	Scanner sc = new Scanner(System.in);
         	System.out.println("Primer archivo: ");
             FileReader fr1 = new FileReader(sc.nextLine());
             System.out.println("Segundo archivo: ");
             FileReader fr2 = new FileReader(sc.nextLine());
-                
             BufferedReader bf1 = new BufferedReader(fr1);
             BufferedReader bf2 = new BufferedReader(fr2);
                 
-            String sCadena1,sCadena2;
+            String Cadena1;
+            String Cadena2;
             boolean iguales = true;         
+            
+            Cadena1 = bf1.readLine();
+            Cadena2 = bf2.readLine();
                 
-            sCadena1 = bf1.readLine();
-            sCadena2 = bf2.readLine();
-                
-            while ((sCadena1!=null) && (sCadena2!=null) && iguales) {
-            	
-                if (!sCadena1.equals(sCadena2)){
+            while ((Cadena1!=null) && (Cadena2!=null) && iguales) {
+                if (!Cadena1.equals(Cadena2)){
                 	iguales = false;
                 }
-                sCadena1 = bf1.readLine();
-                sCadena2 = bf2.readLine();    
+                Cadena1 = bf1.readLine();
+                Cadena2 = bf2.readLine();
             }
             bf1.close();
             bf2.close();
             sc.close();
-                
-            if ((iguales) && (sCadena1==null) && (sCadena2==null))
-                System.out.println("Los ficheros son iguales");
-            else
-                System.out.println("Los ficheros son diferentes");
-                        
-                 
-        } catch (FileNotFoundException e){
+            if ((iguales) && (Cadena1==null) && (Cadena2==null)){
+            	System.out.println("Los ficheros son iguales");
+            }else{
+            	System.out.println("Los ficheros son diferentes");
+            }
+        }catch (FileNotFoundException e){
                 e.getMessage();
-        } catch (IOException e){
+        }catch (IOException e){
                 e.getMessage();
         }
 	}
--- ficheros/src/ejercicio5/Ejercicio5.java
+++ ficheros/src/ejercicio5/Ejercicio5.java
@@ -1,5 +1,39 @@
 package ejercicio5;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		File f;
+		int lineas = 0;
+		
+		try {
+			System.out.println("Escriba el nombre del archivo: ");
+			String archivo = sc.nextLine();
+		    f = new File(archivo);
+		    if (f.exists()){
+				Scanner sc2 = new Scanner(f);
+				while (sc2.hasNextLine()){
+					if (lineas<5){
+						System.out.println(sc2.nextLine());
+						lineas++;
+					}else{
+						System.out.println("pulsa enter para continuar: ");
+						sc.nextLine();
+						lineas = 0;
+					}
+				}
+				sc2.close();
+			}
+			else
+				System.out.println("error");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	sc.close();
+	}
 }
